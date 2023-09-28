6098a99d8994206ba41a72cc6111c43f43948390
Yana K <95872119+yana-kovrygina@users.noreply.github.com>
2023-09-28 20:22:49 +0400

    Update task_3.py

--- lesson-4/task_3.py
+++ lesson-4/task_3.py
@@ -6,10 +6,12 @@
 
 for i in range(0, 101):
     print(i)
-    n = input('Should we break? ')
+    while True:
+        n = input('Should we break? ')
+            if n not in ('yes', 'no'):
+                print("Don't understand you")
+        else
+            break
     if n == 'yes':
         break
-    if n == 'no':
-        continue
-    else:
-        print("Don't understand you")
+        
